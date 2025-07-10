7ef75642f5c11a8f4450369ae8bf9441ed019821
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-10 17:39:25 +0500

    refactor: update font styles and colors, remove unused assets, and add authentication page

---
+++ client/app/auth/page.tsx
@@ -0,0 +1,19 @@
+import { FaTelegram } from "react-icons/fa";
+
+const AuthPage = () => {
+  return (
+    <div className="container max-w-md w-full h-screen flex justify-center items-center flex-col space-y-4">
+      <FaTelegram size={120} className="text-blue-500" />
+      <div>
+        <h1 className="text-4xl font-bold">Telegram</h1>
+      </div>
+      <p className="text-center text-muted-foreground text-sm">
+        Telegram is a messaging app with a focus on speed and security, it{"'"}s
+        super-fast, simple and free
+      </p>
+      
+    </div>
+  );
+};
+
+export default AuthPage;
