b9d4db2afe20c657bf4cca5c66f2ecb9f232ae58
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-11 22:49:45 +0500

    feat: implement AddContact component with UI elements for adding contacts

--- client/app/(chat)/_components/add-contact.tsx
+++ client/app/(chat)/_components/add-contact.tsx
@@ -1,8 +1,13 @@
+import { FaTelegram } from "react-icons/fa";
+
 const AddContact = () => {
   return (
     <div className="h-screen w-full flex z-40 relative">
       <div className="flex justify-center items-center z-50 w-full">
-        <div className="flex flex-col items-center gap-4"></div>
+        <div className="flex flex-col items-center gap-4">
+          <FaTelegram size={120} className="dark:text-blue-400 text-blue-500" />
+          <h1>Add contact to start chatting</h1>
+        </div>
       </div>
     </div>
   );
