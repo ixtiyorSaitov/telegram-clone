ad6ef522e7a5d179cec5a841476768e9648ef8b2
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-13 12:57:30 +0500

    middleware & user model

--- server/app.js
+++ server/app.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const { default: mongoose } = require("mongoose");
+const errorMiddleware = require("./middlewares/error.middleware");
 
 const app = express();
 
-app.use(express.json())
-
-const PORT = process.env.PORT || 6000;
+app.use(express.json());
 
 app.use("/api", require("./routes/index"));
 
-app.listen(PORT, () => console.log("Server is running on port 4000"));
+app.use(errorMiddleware)
+
+const PORT = process.env.PORT || 6000;
+
+const bootstrap = async () => {
+  try {
+    mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log("Connected to MongoDB"));
+    app.listen(PORT, () => console.log("Server is running on port 4000"));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+bootstrap();
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,18 @@
+const userModel = require("../models/user.model");
+
 class AuthController {
   async login(req, res, next) {
-    const { email, password } = req.body;
-    res.json({ email });
+    try {
+      const { email, password } = req.body;
+      const existUser = await userModel.findOne({ email });
+      if (existUser) {
+        return res.status(400).json({ message: "Email already exists" });
+      }
+      const createdUser = await userModel.create({ email });
+      res.status(201).json(createdUser);
+    } catch (error) {
+      next(error);
+    }
   }
 
   async verify(req, res, next) {
