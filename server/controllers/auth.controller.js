f2ed80e9d8da6c6b798dd41d95abf8d69198262c
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-13 13:38:28 +0500

    SMTP

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,18 +1,20 @@
 const BaseError = require("../errors/base.error");
 const userModel = require("../models/user.model");
+const mailService = require("../service/mail.service");
 
 class AuthController {
   async login(req, res, next) {
     try {
       const { email, password } = req.body;
-      const existUser = await userModel.findOne({ email });
-      if (existUser) {
-        throw BaseError.BadRequest("User already exists", [
-          { email: "Email already exists" },
-        ]);
-      }
-      const createdUser = await userModel.create({ email });
-      res.status(201).json(createdUser);
+      await mailService.sendOtp(email)
+      // const existUser = await userModel.findOne({ email });
+      // if (existUser) {
+      //   throw BaseError.BadRequest("User already exists", [
+      //     { email: "Email already exists" },
+      //   ]);
+      // }
+      // const createdUser = await userModel.create({ email });
+      res.status(201).json({email});
     } catch (error) {
       next(error);
     }
