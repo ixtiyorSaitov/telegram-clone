1256616347592722c7f1707e17dc34592c54f9bd
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-18 23:58:43 +0500

    reactions

--- client/components/cards/message.card.tsx
+++ client/components/cards/message.card.tsx
@@ -52,6 +52,7 @@ const MessageCard: FC<Props> = ({ message }) => {
             </div>
           </div>
         </div>
+        
       </ContextMenuTrigger>
       <ContextMenuContent className="w-56 p-0 mb-10">
         <ContextMenuItem className="grid grid-cols-5">
